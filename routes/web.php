660ffca423d3aa5553b3f747470a25cf319c162f
irahayuningtias <2031710085@student.polinema.ac.id>
2023-05-28 18:51:14 +0700

    revisi route

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,42 @@ Route::get('/contact', function () {
 Route::get('/login', function () {
     return View::make('admin/login');
 });
+Route::get('/dashboard', function(){
+    return View::make('admin/dashboard');
+});
+Route::get('/pasien', function(){
+    return View::make('admin/pasien');
+});
+Route::get('/pasien/form-pasien', function(){
+    return View::make('admin/form-pasien');
+});
+Route::get('/obat', function(){
+    return View::make('admin/obat');
+});
+Route::get('/obat/form-obat', function(){
+    return View::make('admin/form-obat');
+});
+Route::get('/alat-medis', function(){
+    return View::make('admin/alat-medis');
+});
+Route::get('/alat-medis/form-alat-medis', function(){
+    return View::make('admin/form-alat-medis');
+});
+Route::get('/rekam-medis', function(){
+    return View::make('admin/rekam-medis');
+});
+Route::get('/rekam-medis/form-rekam-medis', function(){
+    return View::make('admin/form-rekam-medis');
+});
+Route::get('/pemasukan', function(){
+    return View::make('admin/pemasukan');
+});
+Route::get('/pemasukan/form-pemasukan', function(){
+    return View::make('admin/form-pemasukan');
+});
+Route::get('/pengeluaran', function(){
+    return View::make('admin/pengeluaran');
+});
+Route::get('/pengeluaran/form-pengeluaran', function(){
+    return View::make('admin/form-pengeluaran');
+});
